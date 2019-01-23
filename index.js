2fd00323adccec164ecf407c3827de1ebed21882
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-01-23 14:02:12 -0700

    Data renders on browser

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+const environment = process.env.NODE_ENV || 'development';
+const config = require('./knexfile')[environment];
+const knex = require('knex')(config);
+
+app.get('/', (req, res, next) => {
+    knex('methods')
+        //knex.select('*').from('methods') is longhand for knex('methods)
+        .then((rows) => {
+            res.send(rows);
+        })
+        .catch((err) => {
+            next(err);
+        });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
