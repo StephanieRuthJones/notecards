69cd54ac496a258b5a7375e53d8a1a8a4a3b3a1d
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-01-25 11:45:17 -0700

    Data entered; migration complete.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const cors = require('cors')
 const environment = process.env.NODE_ENV || 'development';
 const config = require('./knexfile')[environment];
 const knex = require('knex')(config);
+
+app.use(cors())
 
 app.get('/', (req, res, next) => {
     knex('methods')
--- migrations/20190125094328_methods.js
+++ migrations/20190125094328_methods.js
@@ -1,8 +1,14 @@
 
+
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('methods', table => {
         table.increments()
+        table.string('syntax').notNullable().defaultsTo('')
         table.string('name').notNullable().defaultsTo('')
+        table.text('description').notNullable().defaultsTo('')
+        table.text('example').notNullable().defaultsTo('')
+        table.text('link').notNullable().defaultsTo('')
+
     })
 };
 
